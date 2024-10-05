628096003d5b1ea568b5ce6e0af66aa2792e230c
Eklas-Mohin <mohin030299@gmail.com>
2024-10-05 16:05:22 +0600

    CSES - Weird Algorithm / Collatz Conjecture

---
+++ 001_weird_algorithm.py
@@ -0,0 +1,22 @@
+''' author: mohin 
+    problem: Collatz conjecture 
+'''
+
+def main():
+    n = int(input())
+    sequence = []
+    
+    while True:
+        sequence.append(str(n))
+        if n == 1:
+            break
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+    
+    print(' '.join(sequence))
+
+if __name__ == '__main__':
+    main()
+    
