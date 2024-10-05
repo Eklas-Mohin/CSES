41fb21b87a2aa90008714db9c24eac436e42bc53
Eklas-Mohin <mohin030299@gmail.com>
2024-10-05 22:20:58 +0600

    CSES - Permutations

---
+++ 005_permutations.py
@@ -0,0 +1,21 @@
+''' author: mohin 
+    problem: abs(list[i] - list[i - 1]) > 1
+'''
+
+def main():
+    n = int(input())
+    permutation = []
+    
+    if n > 1 and n < 4:
+        print('NO SOLUTION')
+    else:
+        for i in range(2, n + 1, 2):
+            permutation.append(str(i))
+            
+        for i in range(1, n + 1, 2):
+            permutation.append(str(i))
+            
+    print(' '.join(permutation))
+    
+if __name__ == '__main__':
+    main()
