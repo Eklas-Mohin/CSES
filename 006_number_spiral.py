a0a0f57cc763e32e44917810ac0c9295d56cefd3
Eklas-Mohin <mohin030299@gmail.com>
2024-10-05 22:28:57 +0600

    CSES - Number Spiral

---
+++ 006_number_spiral.py
@@ -0,0 +1,21 @@
+''' author: mohin 
+    problem: number spiral
+'''
+
+def main():
+    t = int(input())
+    
+    for _ in range(t):
+        x, y = map(int, input().split())
+        
+        if y % 2 == 1 and y >= x:
+            print(y * y - x + 1)
+        elif x % 2 == 1 and x > y:
+            print((x - 1) * (x - 1)  + y)
+        elif x % 2 == 0 and x >= y:
+            print(x * x - y + 1)
+        else:
+            print((y - 1) * (y - 1)  + x)
+
+if __name__ == '__main__':
+    main()
