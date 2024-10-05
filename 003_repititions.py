6e9fd2dffbde8e1116697596b64466efd0a9404f
Eklas-Mohin <mohin030299@gmail.com>
2024-10-05 17:04:44 +0600

    CSES - Maximum Repititions of consecutive characters

---
+++ 003_repititions.py
@@ -0,0 +1,20 @@
+''' author: mohin 
+    problem: maximum repetitions of consecutive characters
+'''
+
+def main():
+    st = input()
+    mx = cnt = 1
+    
+    for i in range(1, len(st)):
+        if st[i] == st[i - 1]:
+            cnt += 1
+            mx = max(mx, cnt)
+        else:
+            cnt = 1
+    
+    print(mx)
+    
+if __name__ == '__main__':
+    main()
+    
