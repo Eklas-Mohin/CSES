8163c79267f339c3ef37ea28b43ee188a9e5bbc4
Eklas-Mohin <mohin030299@gmail.com>
2024-10-14 11:47:11 +0600

    CSES - Missing Number Python Solution

--- 002_missing_number.py
+++ 002_missing_number.py
@@ -1,13 +1,11 @@
-''' author: mohin 
-    problem: sum of series(1 + 2 + ... + n) 
-'''
+# 1 + 2 + 3 + .... + n = n * (n + 1) / 2
 
 def main():
     n = int(input())
     nums = list(map(int, input().split()))
     
-    sum_n = n * (n + 1) // 2
     x = sum(nums)
+    sum_n = n * (n + 1) // 2
     
     print(sum_n - x)
     
