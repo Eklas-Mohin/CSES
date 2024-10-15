8cae9ee10a1dfc63c0eae74dbecedd61b735e233
Eklas-Mohin <mohin030299@gmail.com>
2024-10-15 15:18:41 +0600

    CSES - Weird Algorithm Java Solution

--- WeirdAlgorithm.java
+++ WeirdAlgorithm.java
@@ -1,3 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class WeirdAlgorithm {
+
+    public static String collatzConjecture(long n) {
+        StringBuilder collatzSequence = new StringBuilder();
+
+        while (n > 1) {
+            collatzSequence.append(n).append(' ');
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+        }
+        collatzSequence.append(n);
+        
+        return collatzSequence.toString();
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out);
+
+        long n = Long.parseLong(reader.readLine());
+        String collatzSequence = collatzConjecture(n);
+        writer.println(collatzSequence);
+
+        writer.flush();
+    }
+}
+                          
+/*************************** Alternative Solutions ******************************                                                
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -32,3 +68,23 @@ public class WeirdAlgorithm {
         writer.flush();
     }
 }
+
+***************************************************************************************
+
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+
+            while (n > 1) {
+                System.out.print(n + ' ');
+            }
+            System.out.println(n);
+        }
+    }
+}
+
+*****************************************************************************************/
